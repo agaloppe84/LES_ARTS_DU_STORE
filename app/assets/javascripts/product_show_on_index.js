1b056f873999955a414ac49a0f758049151f9282
aurelien galoppe <agaloppe@me.com>
2016-10-20 10:16:22 +0200

    more ajax and js

---
+++ app/assets/javascripts/product_show_on_index.js
@@ -0,0 +1,12 @@
+// $('#productShowOnIndex').on('show.bs.modal', function (event) {
+//     var $modal = $(this);
+//     var button = $(event.relatedTarget);
+//     var productId = button.data('productid');
+//     var typeId = button.data('typeid');
+//     $.ajax({
+//       url: "types/" + typeId + "/products/" + productId +,
+//       success: function(data){
+//           $modal.find('.modal-body').html(data)
+//       }
+//     });
+// })
