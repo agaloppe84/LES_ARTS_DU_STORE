ae70e5fd53e7d90c4231cd144c18f7c7cfce9ac9
aurelien galoppe <agaloppe@me.com>
2016-11-13 14:34:09 +0100

    js on tabs works fine

--- app/assets/javascripts/test_with_tabs.js
+++ app/assets/javascripts/test_with_tabs.js
@@ -1,3 +1,12 @@
-// $('.close-modal').click(function() {
-//     $('.modal-backdrop.in').remove();
-// });
+$('.reset-active-on-tab').click(function() {
+    $('.tab-to-reset').removeClass('active');
+});
+
+$('.reset-active-on-tab').click(function() {
+    $('.other-active-to-remove').removeClass('active');
+});
+
+
+$('.add-active-to-tab').click(function() {
+    $('.tab-to-active').addClass('active');
+});
